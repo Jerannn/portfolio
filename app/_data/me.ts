2a7e9cc243dfe42a8c52b51ceb2fe8707b393cdf
Jerannn <jerandeduro16@gmail.com>
2026-07-27 19:55:18 +0800

    feat: add experience and modify some files

---
+++ app/_data/me.ts
@@ -0,0 +1,17 @@
+export const me = {
+  name: "Jeran Peñaredonda Deduro",
+  avatar: "/me.jpg",
+  title: "Full Stack Web Developer",
+  experience: "3 years",
+  projects: "1",
+  links: [
+    {
+      label: "Github",
+      href: "https://github.com/Jerannn",
+    },
+    {
+      label: "LinkedIn",
+      href: "https://www.linkedin.com/in/jeran-deduro/",
+    },
+  ],
+};
